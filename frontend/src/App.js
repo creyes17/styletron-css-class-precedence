8a5e1b7a552d05a5919c1c233ac646cfbf30ee5f
Christopher R. Reyes <ChrisReyes@alum.mit.edu>
2020-03-23 10:49:39 -0700

    Cleanup warning

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, NavLink} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import {Client} from 'styletron-engine-atomic';
 import {Provider as StyletronProvider} from 'styletron-react';
 
